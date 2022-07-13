db26f3687d03b05b942fd95f2732ef8466c169cf
Julia720 <106229207+ose720@users.noreply.github.com>
2022-07-13 14:54:51 +0900

    Add files via upload

---
+++ example_20220712_highwayChangeDriver/20200710Test/src/main/highwayChange/Car.java
@@ -0,0 +1,19 @@
+package highwayChange;
+
+import highwayChange.driver.*;
+
+public class Car {
+    private Driver driverType;
+
+    public Car() {
+        driverType = new Beginner();
+    }
+
+    public void setDriver(Driver input_driver) {
+        driverType = input_driver;
+        System.out.println("드라이버가 교체 되었습니다 : " + driverType.getClass().getName());
+    }
+    public Driver getDriver() {
+        return driverType;
+    }
+}
