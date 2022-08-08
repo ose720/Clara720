561b0e415d83f6f6b1c67d4ecbd030d8f5f22d99
Julia720 <106229207+ose720@users.noreply.github.com>
2022-08-08 11:19:20 +0900

    Add files via upload

---
+++ Spring_boot_220805_OK/src/main/java/com/example/initproject/domain/User.java
@@ -0,0 +1,16 @@
+//package com.example.initproject.domain;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Table(name="customers")
+//public class User {
+//
+//    @Id
+//    @Column(name = "customer_id")
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    private String name;
+//    private String email;
+//}
---
+++ Spring_boot_220805_OK/src/test/java/com/example/initproject/InitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.initproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
